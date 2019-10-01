ccdc11199cae15379da25fb70dc58569bae95e94
mthipparthi <mahesh.thipparthi@isentia.com>
2019-10-01 14:15:35 +1000

    made templatizable

--- {{cookiecutter.repo_name}}/tests/test_config.py
+++ {{cookiecutter.repo_name}}/tests/test_config.py
@@ -2,4 +2,4 @@ from context import {{cookiecutter.module_name}}
 
 
 def test_config():
-    assert {{cookiecutter.module_name}}.config.config_1 == "config_val_2"
+    assert {{cookiecutter.module_name}}.config.config_1 == "config_val_1"
